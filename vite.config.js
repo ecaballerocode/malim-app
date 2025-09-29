5f3b47c599898dece0745c5291381871b2c7ce0e
Eder <ochoahudson@gmail.com>
2025-09-29 23:35:45 +0000

    vercel corregido

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', 
   // Si quieres que el servidor de desarrollo funcione correctamente en Codespaces/contenedores
   server: {
     host: true, 
